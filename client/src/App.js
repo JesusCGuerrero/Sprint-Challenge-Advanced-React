71e051cac150a30112086756aa7c4467df635057
Jesus C Guerrero <jesusclementguerrero@gmail.com>
2020-04-10 13:25:33 -0400

    mvp and 2 hooks stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import Card from './components/Card'
+import Nav from './components/Nav'
+import './App.css'
 
-function App() {
-  return (
-    <div>
 
-    </div>
-  );
+class App extends React.Component {
+  constructor () {
+      super();
+      this.state = {
+          card: []
+      }
+  }
+
+  componentDidMount() {
+      axios.get(`http://localhost:5000/api/players`)
+      .then(response => {
+          console.log(response.data)
+          this.setState({
+              card: response.data
+          })
+      })
+  }
+
+  render() {
+
+    console.log(`card:`, this.state.card)
+
+      return (
+          <div className="myApp">
+
+            <Nav />
+
+            <div className="cardBox">
+              {this.state.card && this.state.card.map((item) => {
+                return <Card name={item.name} country={item.country} searches={item.searches}/>
+              })}
+            </div>
+
+          </div>
+      )
+  }
 }
 
 export default App;
